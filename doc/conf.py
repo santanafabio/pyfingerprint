c570e1ce5688e6ee596e55255b954bc1b0a96d33
Philipp Meisberger <team@pm-codeworks.de>
2019-02-17 15:51:17 +0100

    Creating API documentation (HTML format) using Sphinx; Introduced python-fingerprint-doc Debian package for offline documentation
    --HG--
    branch : Feature/api-documentation

---
+++ doc/conf.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+project = u'PyFingerprint'
+master_doc = 'PyFingerprint'
+copyright = '2019, Bastian Raschke <bastian.raschke@posteo.de>'
+author = 'Bastian Raschke <bastian.raschke@posteo.de>'
+version = '1.5'
+release = '1.5'
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store'
+]
+extensions = [
+    'sphinx.ext.autodoc',
+]
