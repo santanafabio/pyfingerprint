a2e88fbb7173106e6cd3bdc8f01b4642e97f99d9
Philipp Meisberger <team@pm-codeworks.de>
2019-02-19 19:06:06 +0100

    Importing version number from module
    --HG--
    branch : Feature/api-documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append('../src/files/')
+
 project = u'PyFingerprint'
 master_doc = 'PyFingerprint'
 copyright = '2019, Bastian Raschke <bastian.raschke@posteo.de>'
 author = 'Bastian Raschke <bastian.raschke@posteo.de>'
-version = '1.5'
-release = '1.5'
+version = __import__('pyfingerprint').__version__
+release = version
 exclude_patterns = [
     '_build',
     'Thumbs.db',
